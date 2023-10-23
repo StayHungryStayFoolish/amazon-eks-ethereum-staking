8e1fb84aee220c1d1d4ef30b9bc837b29e7f8419
Bonismo <bonismo@amazon.com>
2023-10-23 13:30:03 +0800

    remove Grafana

--- lib/observe.ts
+++ lib/observe.ts
@@ -5,7 +5,7 @@ import * as eks from 'aws-cdk-lib/aws-eks';
 import * as iam from 'aws-cdk-lib/aws-iam';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as kms from 'aws-cdk-lib/aws-kms';
-import { aws_grafana as grafana } from 'aws-cdk-lib';
+// import { aws_grafana as grafana } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { RemovalPolicy } from 'aws-cdk-lib';
 import { CfnJson } from 'aws-cdk-lib';
